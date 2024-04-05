dcdf92f65add1e9fa540ac47b90dcc74de5e40f5
Gun Gun Priatna <gungun002@ummi.ac.id>
2024-04-05 10:15:49 +0700

    add laravel ui

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/messages', [HomeController::class, 'messages'])
+    ->name('messages');
+Route::post('/message', [HomeController::class, 'message'])
+    ->name('message');
