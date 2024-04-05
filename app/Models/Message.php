ce865d548102ec09ed9fa690a57b05cceebe4db1
Gun Gun Priatna <gungun002@ummi.ac.id>
2024-04-05 10:13:18 +0700

    add model and migration files

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'text'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getTimeAttribute(): string
+    {
+        return date('d M Y, H:i:s', strtotime($this->attributes['created_at']));
+    }
 }
